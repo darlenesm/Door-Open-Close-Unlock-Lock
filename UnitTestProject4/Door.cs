4875ded769f408081ed00fcd7c3ffc39a6a34bba
darlenesm <72404983+darlenesm@users.noreply.github.com>
2021-11-29 21:32:30 -0400

    Add files via upload

---
+++ UnitTestProject4/Door.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTestProject4
+{
+    class Door
+    {
+        public static bool Open()      
+        {
+            return true;
+        }
+
+        public static bool Close ()
+        {
+            return true; 
+        }
+
+        public static bool Lock ()
+        {
+            return true; 
+        }
+
+        public static bool unlock (float key)
+        {
+            return true;
+        }
+    }
+
+}
---
+++ UnitTestProject4/UnitTest1.cs
@@ -0,0 +1,20 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject4
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void DoorOpenToOpen()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void DoorClosedToClosed()
+        {
+            Assert.Fail();
+        }
+    }
+}
